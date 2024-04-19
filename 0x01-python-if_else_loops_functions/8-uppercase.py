082f169617db6b8ac116d060eef60e126203271d
Hendzaineddin <hendzad.32@gmail.com>
2024-04-19 13:42:41 -0700

    l

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 i = 0
 while(str(i) != '\0'):
     if (ord(str(i)) >= 97) and (ord(str(i)) <= 122):
-        print(str(i) + "A")
+        print("{}".format(str(i) + "A"))
     else:
         continue
