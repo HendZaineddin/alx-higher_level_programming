4bd0738c1b28269201ac75093a2e365134f9fc69
Hendzaineddin <hendzad.32@gmail.com>
2024-04-19 15:11:08 -0700

    l

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def pow(a, b):
 k = 0
-k = a ^ b
+if b == 0:
+    return (1)
+elif b < 0:
+    b = b + -1
+    for i in range(b):
+        k += a / a
+elif b > 0:
+    for i in range(b):
+        k += a * a
+if a < 0:
+    return (-k)
 return (k)
