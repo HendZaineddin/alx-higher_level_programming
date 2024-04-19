2e0169f3804d738adfcfd99f8b832d0be142778d
Hendzaineddin <hendzad.32@gmail.com>
2024-04-19 11:50:04 -0700

    l

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-k=1
-for i in range (10):
-    while(k < 10):
-        if(i < 8):
+k = 1
+for i in range(10):
+    while (k < 10):
+        if (i < 8):
             print("{}{}, ".format(i, k), end="")
         else:
             print("{}{}".format(i, k), end="\n")
-        k+=1
-    k=i+2
+        k += 1
+    k = i + 2
